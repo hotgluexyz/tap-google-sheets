7d741aaadac045c2300211e20cd76626f015342c
Mohammad Areeb Siddiqui <mohammadareebs@gmail.com>
2025-08-14 18:53:01 +0500

    hgi-8594: discover metadata streams only once

--- tap_google_sheets/discover.py
+++ tap_google_sheets/discover.py
@@ -4,14 +4,15 @@ from tap_google_sheets.schema import STREAMS
 
 def discover(client,spreadsheet_ids):
     catalog = Catalog([])
+    discovered_metadata_streams = []
     for spreadsheet_id in spreadsheet_ids:
         for stream, stream_obj in STREAMS.items():
             stream_object = stream_obj(client, spreadsheet_id.get("id"))
             schemas, field_metadata = stream_object.get_schemas()
-            ignore_list = ["file_metadata","spreadsheet_metadata","sheet_metadata","sheets_loaded"]
+            metadata_stream_list = ["file_metadata","spreadsheet_metadata","sheet_metadata","sheets_loaded"]
             # loop over the schema and prepare catalog
             for stream_name, schema_dict in schemas.items():
-                if stream_name not in ignore_list:
+                if stream_name not in metadata_stream_list:
                    standard_name = ''.join(x for x in spreadsheet_id.get('name').title() if not x.isspace())
                    stream_name_new = f"{standard_name}_{stream_name}"
                    field_metadata[stream_name_new]=  field_metadata[stream_name]
@@ -33,6 +34,11 @@ def discover(client,spreadsheet_ids):
                 else:
                     stream_obj = STREAMS.get(stream_name)(client, spreadsheet_id)
                     key_props = stream_obj.key_properties
+
+                if stream_name in discovered_metadata_streams:
+                    continue
+                elif stream_name in metadata_stream_list:
+                    discovered_metadata_streams.append(stream_name)
 
                 catalog.streams.append(CatalogEntry(
                     stream=stream_name,
